2b81bd7d9ed8e13f33b127cebfb1c3bdac82738d
corneliajohnson <corneliaj91@gmail.com>
2021-07-11 08:11:52 -0500

    added for convo

--- Program.cs
+++ Program.cs
@@ -9,6 +9,9 @@ namespace Intro
       Console.Write("What is your name? ");
       string name = Console.ReadLine();
       Console.WriteLine($"Hello, {name}! I'm glad to meet you.");
+      Console.WriteLine("What are your plans today? ");
+      string plans = Console.ReadLine();
+            Console.WriteLine($"Nice! Hope you have a good time while you {plans}");
     }
   }
 }
