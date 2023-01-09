88170a8aa8003074c28ab247393b384841433b76
corneliajohnson <corneliaj91@gmail.com>
2023-01-09 07:58:33 -0600

    changed message

--- Program.cs
+++ Program.cs
@@ -6,12 +6,18 @@ namespace Intro
   {
     static void Main(string[] args)
     {
-      Console.Write("What is your name? ");
-      string name = Console.ReadLine();
-      Console.WriteLine($"Hello, {name}! I'm glad to meet you.");
-      Console.WriteLine("What are your plans today? ");
-      string plans = Console.ReadLine();
-            Console.WriteLine($"Nice! Hope you have a good time while you {plans}");
-    }
+            System.Console.Write("Who would you like to say hello to? ");
+
+            string name = System.Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                System.Console.WriteLine("Fine, don't say 'hello'!");
+            }
+            else
+            {
+                System.Console.WriteLine($"Hello, {name}!");
+            }
+        }
   }
 }
